3cb7088ade323b790e7538e5e2d7104673029c96
Ben Darcy <bendarcy22@gmail.com>
2016-08-27 12:29:29 -0400

    react redux tutorial

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from 'redux';
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <p>"Hello, World!"</p>,
+  document.getElementById('app')
+)
