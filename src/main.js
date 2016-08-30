bdf6f684063798822bcfee667ae1d8ca48ca1863
Ben Darcy <bendarcy22@gmail.com>
2016-08-30 17:03:56 -0400

    making changes without mutations

--- src/main.js
+++ src/main.js
@@ -1,55 +1,104 @@
-import { combineReducers } from 'redux';
-import React, { PropTypes } from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { createStore, combineReducers } from 'redux';
+// import React, { PropTypes } from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+//
+// const store = createStore(counter);
+//
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
 
+const addCounter = (list) => {
+  return [...list, 0];
+};
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter(l = l !== listener);
-    };
-  };
-
-  dispatch({});
-
-  return { getState, dispatch, subscribe };
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ];
 }
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
 }
 
-const store = createStore(counter);
-
-const render = () => {
-  document.body.innerText = store.getState();
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+  deepFreeze(listBefore);
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
 }
 
-store.subscribe(render);
-render();
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+  deepFreeze(listBefore);
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+}
 
-document.addEventListener('click', () =>{
-  store.dispatch({ type: 'INCREMENT' });
-});
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+  deepFreeze(listBefore);
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+}
 
-// ReactDOM.render(
-//   <p>"Hello, World!"</p>,
-//   document.getElementById('app')
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed.')
+
+// const Counter = ({
+//   value,
+//   onIncrement,
+//   onDecrement
+// }) => (
+//   <div>
+//     <h1>{value}</h1>
+//     <button onClick={onIncrement}>+</button>
+//     <button onClick={onDecrement}>-</button>
+//   </div>
 // )
+//
+// const render = () => {
+//   ReactDOM.render(
+//     <Counter
+//       value={store.getState()}
+//       onIncrement={() =>
+//         store.dispatch({
+//           type: 'INCREMENT'
+//         })
+//       }
+//       onDecrement={() =>
+//         store.dispatch({
+//           type: 'DECREMENT'
+//         })
+//       }
+//       />,
+//     document.getElementById('app')
+//   );
+// }
+//
+// store.subscribe(render);
+// render();
